13215f8cd342d1526efea511d4d4ca410e4db8bb
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-12-14 14:21:23 +0200

    Create darkTheme and searchTerm context state

--- src/context.jsx
+++ src/context.jsx
@@ -3,7 +3,18 @@ import { createContext, useContext, useState, useEffect } from 'react';
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const value = {};
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('horse');
+
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    const body = document.querySelector('body');
+    body.classList.toggle('dark-theme', newDarkTheme);
+  };
+
+  const value = { isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm };
+
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
