d1ea026560dc2470446441dbf33a7b475e2598c4
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-12-14 14:23:07 +0200

    Add search term functionality

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { useGlobalContext } from '../context';
 
 const SearchForm = () => {
-  return <h2>SearchForm</h2>;
+  const { setSearchTerm } = useGlobalContext();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    setSearchTerm(searchValue);
+  };
+  return (
+    <section>
+      <h1 className='title'>Unsplash Images</h1>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='search'
+          placeholder='Search'
+          className='form-input search-input'
+        />
+        <button type='submit' className='btn'>
+          Search
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
