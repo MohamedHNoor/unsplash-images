1d6565fb7d701d3d67a05477aba8c2f7071f5ec7
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-12-18 18:20:43 +0200

    Create dark mode javaScript check and save it to local storage

--- src/context.jsx
+++ src/context.jsx
@@ -2,16 +2,28 @@ import { createContext, useContext, useState, useEffect } from 'react';
 
 const AppContext = createContext();
 
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    'prefers-color-scheme:dark'
+  ).matches;
+  const storedDarkMode = localStorage.getItem('darkTheme') === 'true';
+
+  return storedDarkMode || prefersDarkMode;
+};
+
 export const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
   const [searchTerm, setSearchTerm] = useState('horse');
 
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
     setIsDarkTheme(newDarkTheme);
-    const body = document.querySelector('body');
-    body.classList.toggle('dark-theme', newDarkTheme);
+    localStorage.setItem('darkTheme', newDarkTheme);
   };
+
+  useEffect(() => {
+    document.body.classList.toggle('dark-theme', isDarkTheme);
+  }, [isDarkTheme]);
 
   const value = { isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm };
 
