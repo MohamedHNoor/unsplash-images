5ecf0ea0d8df63dbf103b4b29e43c2571e125f7b
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-12-18 18:29:17 +0200

    Add env to gitignore and update the api url

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,8 +3,9 @@ import axios from 'axios';
 import React from 'react';
 import { useGlobalContext } from '../context';
 
-const url =
-  'https://api.unsplash.com/search/photos?client_id=uT8i03-dsUfAx2rdCQ4zDbG2CDWKwOTpCqhfUu57E7I';
+const url = `https://api.unsplash.com/search/photos?client_id=${
+  import.meta.env.VITE_API_KEY
+}`;
 
 const Gallery = () => {
   const { searchTerm } = useGlobalContext();
